96c2235513d1acbce600a08d515ea933f56df732
ymrtkhr <96984306+ymrtkhr@users.noreply.github.com>
2022-06-20 17:26:53 +0900

    TODO_JavaScript

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-// 論理演算子の本当の意味をしろう
+import "./styles.css";
 
-const flag1 = true;
-const flag2 = false;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
 
-// if (flag1 || flag2){
-//   console.log('1か2はtrueになります')
-// }
-
-// if (flag1 && flag2){
-//   console.log('1も2もtureになります')
-// }
-
-// ||は左側がfalseなら右側を返す
-const num = 100;
-const fee = num || "金額未設定です";
-console.log(fee);
-
-// &&は左側がtrueなら右側を返す
-const num2 = null;
-const fee2 = num2 && "何かが設定されました";
-console.log(fee2);
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
